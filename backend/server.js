b39f176dfcd1c6b504ac63c0a90d59613f429906
mayurlalwani <mayur_lalwani4@ymail.com>
2021-09-14 17:33:17 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -6,18 +6,28 @@ const app = express();
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+// Deployment
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", () => (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
